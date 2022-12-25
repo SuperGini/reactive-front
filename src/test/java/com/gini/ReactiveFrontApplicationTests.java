24dbd979ea13256188233310c5fc417132068e58
iordache <faraonu_ginitoru@yahoo.com>
2022-12-25 22:07:40 +0200

    initializing project

---
+++ src/test/java/com/gini/ReactiveFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
